e8425ea5219015d175f9c7a246fdba9779fcefcb
gabriel-villabon <g.villabon@uniandes.edu.co>
2022-06-02 17:46:34 -0500

    Correccion espacios ventanamenuproyectos

--- Interfaz/VentanaMenuProyectos.java
+++ Interfaz/VentanaMenuProyectos.java
@@ -13,7 +13,8 @@ import Mundo.Aplicacion.MenuProyectos;
 import Mundo.Proyectos.Proyecto;
 
 
-public class VentanaMenuProyectos extends JFrame{
+public class VentanaMenuProyectos extends JFrame
+{
     MenuProyectos menuProyectos;
     MenuActividades menuActividades;
     PMenuProyectos pMenuProyectos;
@@ -40,7 +41,8 @@ public class VentanaMenuProyectos extends JFrame{
     public final String FINICIO = "F_INICIO";
     public final String FFIN = "F_FIN";
 
-    VentanaMenuProyectos(MenuProyectos menuProyectos, Proyecto proyecto) throws FileNotFoundException, IOException{
+    VentanaMenuProyectos(MenuProyectos menuProyectos, Proyecto proyecto) throws FileNotFoundException, IOException
+    {
         this.menuProyectos = menuProyectos;
         this.proyecto = proyecto;
         pMenuProyectos = new PMenuProyectos(this, proyecto);
@@ -61,8 +63,10 @@ public class VentanaMenuProyectos extends JFrame{
         this.add(pMenuProyectos, BorderLayout.CENTER);
     }
 
-    public void cambiarPanel(String comando) throws FileNotFoundException, IOException {
-        if (comando == MENU){
+    public void cambiarPanel(String comando) throws FileNotFoundException, IOException
+    {
+        if (comando == MENU)
+        {
             this.remove(pElegirParticipante);
             this.remove(pDescripcion);
             this.remove(pParticipantes);
@@ -72,96 +76,117 @@ public class VentanaMenuProyectos extends JFrame{
             this.remove(pAgregaTipoActividad);
             this.remove(pAgregarFechaF);
             this.add(pMenuProyectos);
-
-        }else if (comando == MENUPPAL){
+        }
+        else if (comando == MENUPPAL)
+        {
             this.setVisible(false);
-
-        }else if (comando == GESTIONAR){
+        }
+        else if (comando == GESTIONAR)
+        {
             this.remove(pMenuProyectos);
             pElegirParticipante = new PElegirParticipante(this, proyecto.getParticipantes());
             this.add(pElegirParticipante);
-            
-        }else if (comando == DESCRIPCION){
+        }
+        else if (comando == DESCRIPCION)
+        {
             this.remove(pMenuProyectos);
             this.add(pDescripcion);
-
-        }else if (comando == PARTICIPANTES){
+        }
+        else if (comando == PARTICIPANTES)
+        {
             pParticipantes = new PParticipantes(this, proyecto);
             this.remove(pMenuProyectos);
             this.add(pParticipantes);
-
-        }else if (comando == FINICIO){
+        }
+        else if (comando == FINICIO)
+        {
             this.remove(pMenuProyectos);
             this.add(pInicio);
-
-        }else if (comando == FFIN ){
+        }
+        else if (comando == FFIN)
+        {
             this.remove(pMenuProyectos);
             this.add(pFin);
-
-        }else if (comando == AGREGAR_PARTICIPANTES ){
+        }
+        else if (comando == AGREGAR_PARTICIPANTES)
+        {
             this.remove(pMenuProyectos);
             this.add(pAgregarParticipante);
-        
-        }else if (comando == AGREGAR_TIPO ){
+        }
+        else if (comando == AGREGAR_TIPO)
+        {
             this.remove(pMenuProyectos);
             this.add(pAgregaTipoActividad);
-        
-        }else if (comando == MODIFICAR_FIN ){
+        }
+        else if (comando == MODIFICAR_FIN)
+        {
             this.remove(pMenuProyectos);
             this.add(pAgregarFechaF);
-        
-        }else if (comando == AGREGAR_P){
-
-        }else{
+        }
+        else if (comando == AGREGAR_P)
+        {
+        }
+        else
+        {
             ElegirParticipante(comando);
         }
         setSize(getWidth()-1, getHeight()-1);
     }
 
-    public void cambiarFechaFinalizacion(Proyecto proyecto, String fecha) throws FileNotFoundException, IOException {
+    public void cambiarFechaFinalizacion(Proyecto proyecto, String fecha) throws FileNotFoundException, IOException
+    {
         Boolean respuesta = menuProyectos.cambiarFechaFinalizacion(proyecto, fecha);
-        if (respuesta==true){
+        if (respuesta==true)
+        {
             JOptionPane.showMessageDialog(pAgregarParticipante, "La fecha fue cambiada exitosamente",
 				"Proyecto Creado", JOptionPane.INFORMATION_MESSAGE);     
         }
-        else {
+        else
+        {
             JOptionPane.showMessageDialog(pAgregarParticipante, "No se pudo cambiar la fecha",
 				"Error", JOptionPane.ERROR_MESSAGE);
         }
         cambiarPanel(MENU);
     }
 
-    public void AgregarParticipante(String nombre, String correo) throws FileNotFoundException, IOException{
+    public void AgregarParticipante(String nombre, String correo) throws FileNotFoundException, IOException
+    {
         Boolean respuesta = menuProyectos.ejecutarAgregarParticipante(proyecto, nombre, correo);
-        if (respuesta==true){
+        if (respuesta==true)
+        {
             JOptionPane.showMessageDialog(pAgregarParticipante, "El participante fue añadido exitosamente",
 				"Proyecto Creado", JOptionPane.INFORMATION_MESSAGE);     
         }
-        else {
+        else
+        {
             JOptionPane.showMessageDialog(pAgregarParticipante, "No se pudo añadir el participante",
 				"Error", JOptionPane.ERROR_MESSAGE);
         }
         cambiarPanel(MENU);
     }
-    public void AgregarTipo(String tipo) throws FileNotFoundException, IOException{
+
+    public void AgregarTipo(String tipo) throws FileNotFoundException, IOException
+    {
         Boolean respuesta = menuProyectos.ejecutarAgregarTipo(proyecto, tipo);
-        if (respuesta==true){
+        if (respuesta==true)
+        {
             JOptionPane.showMessageDialog(pAgregarParticipante, "El tipo fue añadido exitosamente",
 				"Proyecto Creado", JOptionPane.INFORMATION_MESSAGE);     
         }
-        else {
+        else
+        {
             JOptionPane.showMessageDialog(pAgregarParticipante, "No se pudo añadir el tipo",
 				"Error", JOptionPane.ERROR_MESSAGE);
         }
         cambiarPanel(MENU);
     }
 
-    public void ElegirParticipante(String boton) throws FileNotFoundException, IOException{
+    public void ElegirParticipante(String boton) throws FileNotFoundException, IOException
+    {
         int num = Integer.parseInt(boton.replace("PARTICIPANTE ", ""));
         menuActividades = new MenuActividades();
         participante = menuActividades.elegirParticipante(proyecto, num);
         cambiarPanel(MENU);
         new VentanaMenuActividades(menuActividades, participante);
     }
-    
 }
